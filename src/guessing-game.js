1b2e900d4557ac5605ca3f5017acd0a714b85c97
elina <elina.nep@gmail.com>
2023-02-04 13:20:20 +0300

    feat guess correctly

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    #min = 0;
+    #max = 0;
+    #guessNumber = 0;
 
-    setRange(min, max) {
 
+    setRange = (min, max) => {
+        this.#min = min;
+        this.#max = max;
     }
 
-    guess() {
-
+    guess = () => {
+        this.#guessNumber = Math.ceil((this.#max + this.#min) / 2)
+        return this.#guessNumber;
     }
 
-    lower() {
-
+    lower = () => {
+        this.#max = this.#guessNumber;
     }
 
-    greater() {
-
+    greater = () => {
+        this.#min = this.#guessNumber;
     }
 }
 
